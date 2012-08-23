84e930b134c1bc2d2bb22074d48a84e80a22cb8d
Jack Bates <jack@nottheoilrig.com>
2012-08-23 06:43:20 -0700

    Switch from jQuery to D3 JavaScript library

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 var skt = new WebSocket('ws://' + location.host + '/skt');
 
-jQuery(function ($)
+skt.onmessage = function (evt)
   {
-    skt.onmessage = function (evt)
+    var delta = JSON.parse(evt.data);
+    delta.forEach(function (itm)
       {
-        var delta = JSON.parse(evt.data);
-        jQuery.each(delta, function ()
-          {
-            var $tr = $('#' + this[0].replace(/\./g, '\\.'));
-            if ($tr.length)
-            {
-              $tr.replaceWith('<tr id="' + this[0] + '"><td>' + this.join('</td><td>') + '</td></tr>');
-            }
-            else
-            {
-              $('<tr id="' + this[0] + '"><td>' + this.join('</td><td>') + '</td></tr>').appendTo('tbody');
-            }
-          });
-      }
-  });
+        // http://www.w3.org/TR/CSS21/syndata#value-def-identifier
+        var tr = d3.select('#\\00003' + itm[0].replace(/\./g, '\\.'));
+        if (tr.empty())
+        {
+          d3.select('tbody').append('tr').attr('id', itm[0]).selectAll('td')
+              .data(itm)
+            .enter().append('td')
+              .text(function (d) { return d; });
+        }
+        else
+        {
+          tr.selectAll('td')
+            .data(itm)
+            .text(function (d) { return d; });
+        }
+      });
+  }
