67ea8ff9ec703e92cc5594d573930103392b80b5
Jack Bates <jack@nottheoilrig.com>
2012-08-25 02:53:24 -0700

    The enter selection merges into the update selection when you append or insert

--- script.js
+++ script.js
@@ -2,23 +2,19 @@ var skt = new WebSocket('ws://' + location.host + '/skt');
 
 skt.onmessage = function (evt)
   {
-    var delta = JSON.parse(evt.data);
-    delta.forEach(function (itm)
+    JSON.parse(evt.data).forEach(function (itm)
       {
         // http://www.w3.org/TR/CSS21/syndata#value-def-identifier
         var tr = d3.select('#\\00003' + itm[0].replace(/\./g, '\\.'));
         if (tr.empty())
         {
-          d3.select('tbody').append('tr').attr('id', itm[0]).selectAll('td')
-              .data(itm)
-            .enter().append('td')
-              .text(function (d) { return d; });
+          tr = d3.select('tbody').append('tr').attr('id', itm[0]);
         }
-        else
-        {
-          tr.selectAll('td')
-            .data(itm)
-            .text(function (d) { return d; });
-        }
+
+        var td = tr.selectAll('td').data(itm);
+
+        td.enter().append('td');
+
+        td.text(function (d) { return d; });
       });
   }
