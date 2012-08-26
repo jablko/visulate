38141c60500ff969b8de457097133927e92ea1ff
Jack Bates <jack@nottheoilrig.com>
2012-08-25 23:55:24 -0700

    Reconnect automatically

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-var skt = new WebSocket('ws://' + location.host + '/skt');
-
-skt.onmessage = function (evt)
+(function connect()
   {
-    JSON.parse(evt.data).forEach(function (itm)
+    var skt = new WebSocket('ws://' + location.host + '/skt');
+
+    skt.onclose = function ()
       {
-        // http://www.w3.org/TR/CSS21/syndata#value-def-identifier
-        var tr = d3.select('#\\00003' + itm[0].replace(/\./g, '\\.'));
-        if (tr.empty())
-        {
-          tr = d3.select('tbody').append('tr').attr('id', itm[0]);
-        }
+        // The first reconnect attempt SHOULD be delayed by a random amount of
+        // time.  The parameters by which this random delay is chosen are left
+        // to the client to decide; a value chosen randomly between 0 and 5
+        // seconds is a reasonable initial delay though clients MAY choose a
+        // different interval from which to select a delay length based on
+        // implementation experience and particular application
 
-        var td = tr.selectAll('td').data(itm);
+        setTimeout(connect, Math.random() * 5000);
+      }
 
-        td.enter().append('td');
+    skt.onmessage = function (evt)
+      {
+        JSON.parse(evt.data).forEach(function (itm)
+          {
+            // http://www.w3.org/TR/CSS21/syndata#value-def-identifier
+            var tr = d3.select('#\\00003' + itm[0].replace(/\./g, '\\.'));
+            if (tr.empty())
+            {
+              tr = d3.select('tbody').append('tr').attr('id', itm[0]);
+            }
 
-        td.text(function (d) { return d; });
-      });
-  }
+            var td = tr.selectAll('td').data(itm);
+
+            td.enter().append('td');
+
+            td.text(function (d) { return d; });
+          });
+      }
+  })();
