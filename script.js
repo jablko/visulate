b23cc7317b1c126bbb95ef1866fde8fb2fbeb9d7
Jack Bates <jack@nottheoilrig.com>
2012-12-27 07:23:16 -0800

    First crack at scatter plot of both client host IP and sever host name

--- script.js
+++ script.js
@@ -50,6 +50,8 @@ function barChart(div)
   {
     var textChi = g.selectAll('text.chi').data(data.slice(begin, begin + 8));
 
+    textChi.exit().remove();
+
     textChi.enter().append('text')
       .attr('class', 'chi')
       .attr('y', function (d, i) { return 20 * i + 14; }); // vertical-align: middle
@@ -65,25 +67,31 @@ function barChart(div)
 
     var rectAll = g.selectAll('rect.all').data(data.slice(begin, begin + 8));
 
+    rectAll.exit().remove();
+
     rectAll.enter().append('rect')
       .attr('class', 'all')
-      .attr('height', 20)
+      .attr('height', 19)
       .attr('x', 6)
-      .attr('y', function (d, i) { return 20 * i; });
+      .attr('y', function (d, i) { return 20 * i + .5; });
 
     rectAll.attr('width', function (d) { return x(psql(all(d))); });
 
     var rectHit = g.selectAll('rect.hit').data(data.slice(begin, begin + 8));
 
+    rectHit.exit().remove();
+
     rectHit.enter().append('rect')
       .attr('class', 'hit')
       .attr('height', 19)
-      .attr('x', 6.5)
+      .attr('x', 6)
       .attr('y', function (d, i) { return 20 * i + .5; });
 
     rectHit.attr('width', function (d) { return x(psql(hit(d))); });
 
     var textPsql = g.selectAll('text.psql').data(data.slice(begin, begin + 8));
+
+    textPsql.exit().remove();
 
     textPsql.enter().append('text')
       .attr('class', 'psql')
@@ -159,13 +167,7 @@ function chart(div)
 
   function my(nstData)
   {
-    data = [];
-    for (var itm in nstData)
-    {
-      data.push([itm, nstData[itm]]);
-    }
-
-    data.sort(function (a, b) { return d3.descending(psql(all(a)), psql(all(b))); });
+    data = nstData;
 
     y.domain([0, psql(all(data[0]))]);
     yAxis.tickValues([0, psql(all(data[0]))]);
@@ -208,7 +210,7 @@ function chart(div)
   return my;
 }
 
-var chiData, pqsnData;
+var chiData, pqsnData, chiPqsnData;
 
 var chiChart = chart(d3.select('body').append('div')),
   pqsnChart = chart(d3.select('body').append('div'));
@@ -249,19 +251,39 @@ var backoff = 0;
         var data = JSON.parse(evt.data);
         if (data[0])
         {
+          // chi
+
           for (var itm in data[0])
           {
             chiData[itm] = data[0][itm];
           }
 
-          chiChart(chiData);
+          var chiSort = [];
+          for (var itm in chiData)
+          {
+            chiSort.push([itm, chiData[itm]]);
+          }
+
+          chiSort.sort(function (a, b) { return d3.descending(psql(all(a)), psql(all(b))); });
+
+          chiChart(chiSort);
+
+          // pqsn
 
           for (var itm in data[1])
           {
             pqsnData[itm] = data[1][itm];
           }
 
-          pqsnChart(pqsnData);
+          var pqsnSort = [];
+          for (var itm in pqsnData)
+          {
+            pqsnSort.push([itm, pqsnData[itm]]);
+          }
+
+          pqsnSort.sort(function (a, b) { return d3.descending(psql(all(a)), psql(all(b))); });
+
+          pqsnChart(pqsnSort);
         }
         else
         {
@@ -284,24 +306,177 @@ var backoff = 0;
 
         d3.json('/open', function (data)
           {
-            chiChart(chiData = data[0]);
+            // chi
 
-            pqsnChart(pqsnData = data[1]);
+            chiData = data[0];
+
+            var chiSort = [];
+            for (var itm in chiData)
+            {
+              chiSort.push([itm, chiData[itm]]);
+            }
+
+            if (chiSort.length)
+            {
+              chiSort.sort(function (a, b) { return d3.descending(psql(all(a)), psql(all(b))); });
+
+              chiChart(chiSort);
+            }
+
+            // pqsn
+
+            pqsnData = data[1];
+
+            var pqsnSort = [];
+            for (var itm in pqsnData)
+            {
+              pqsnSort.push([itm, pqsnData[itm]]);
+            }
+
+            if (pqsnSort.length)
+            {
+              pqsnSort.sort(function (a, b) { return d3.descending(psql(all(a)), psql(all(b))); });
+
+              pqsnChart(pqsnSort);
+            }
+
+            // chiPqsn
+
+            chiPqsnData = data[2];
+
+            var chiPqsnSort = [];
+            chiSort.slice(0, 8).forEach(function (chiItm)
+              {
+                pqsnSort.slice(0, 8).forEach(function (pqsnItm)
+                  {
+                    if (chiPqsnData[chi(chiItm)][pqsn(pqsnItm)])
+                    {
+                      chiPqsnSort.push([chi(chiItm), pqsn(pqsnItm), chiPqsnData[chi(chiItm)][pqsn(pqsnItm)]]);
+                    }
+                  });
+              });
+
+            chiPqsnSort.sort(function (a, b) { return d3.descending(psql(a[2][0]), psql(b[2][0])); });
+
+            var svg = d3.select('body').append('div').attr('id', 'chiPqsn').append('svg'), g = svg.append('g');
+
+            var max = 0;
+            d3.values(chiPqsnData).forEach(function (itm)
+              {
+                d3.values(itm).forEach(function (itm)
+                  {
+                    max = Math.max(max, psql(itm[0]));
+                  });
+              });
+
+            var r = d3.scale.sqrt()
+              .domain([0, max])
+              .range([0, 80]);
+
+            var rBottom = rTop = 0;
+            chiSort.slice(0, 8).forEach(function (itm)
+              {
+                if (chiPqsnData[chi(itm)][pqsn(pqsnSort[0])])
+                {
+                  rBottom = Math.max(rBottom, r(psql(chiPqsnData[chi(itm)][pqsn(pqsnSort[0])][0])));
+                }
+
+                if (chiPqsnData[chi(itm)][pqsn(pqsnSort[7])])
+                {
+                  rTop = Math.max(rTop, r(psql(chiPqsnData[chi(itm)][pqsn(pqsnSort[7])][0])));
+                }
+              });
+
+            var rLeft = rRight = 0;
+            pqsnSort.slice(0, 8).forEach(function (itm)
+              {
+                if (chiPqsnData[chi(chiSort[0])][pqsn(itm)])
+                {
+                  rLeft = Math.max(rLeft, r(psql(chiPqsnData[chi(chiSort[0])][pqsn(itm)][0])));
+                }
+
+                if (chiPqsnData[chi(chiSort[7])][pqsn(itm)])
+                {
+                  rRight = Math.max(rRight, r(psql(chiPqsnData[chi(chiSort[7])][pqsn(itm)][0])));
+                }
+              });
+
+            var marginTop = rTop, marginRight = rRight;
+
+            // Add the x-axis
+            var x = d3.scale.ordinal()
+              .domain(chiSort.slice(0, 8).map(chi))
+              .rangePoints([0, 320 - marginTop - rBottom]);
+
+            var xAxis = d3.svg.axis().scale(x);
+
+            var gXAxis = g.append('g')
+              .attr('class', 'x axis')
+              .call(xAxis);
+
+            gXAxis.selectAll('text').attr('transform', 'rotate(30)');
+
+            var marginBottom = gXAxis.node().getBBox().height;
+
+            // Add the y-axis
+            var y = d3.scale.ordinal()
+              .domain(pqsnSort.slice(0, 8).map(pqsn))
+              .rangePoints([320 - marginTop - rBottom - marginBottom, 0]);
+
+            var yAxis = d3.svg.axis().orient('left').scale(y);
+
+            var gYAxis = g.append('g')
+              .attr('class', 'y axis')
+              .attr('transform', 'translate(' + -rLeft + ')')
+              .call(yAxis);
+
+            var marginLeft = gYAxis.node().getBBox().x;
+
+            if (marginTop < gYAxis.node().getBBox().y)
+            {
+              marginTop = gYAxis.node().getBBox().y;
+
+              y.rangePoints([320 - marginTop - rBottom - marginBottom, 0]);
+              gYAxis.call(yAxis);
+            }
+
+            x.rangePoints([0, 320 - marginTop - rBottom - marginBottom]);
+            gXAxis.attr('transform', 'translate(0,' + (320 - marginTop - marginBottom) + ')').call(xAxis);
+
+            var fill = d3.scale.linear()
+              .domain([0, 1])
+              .range(['#58b', '#f60']);
+
+            var circle = g.selectAll('circle').data(chiPqsnSort);
+
+            circle.exit().remove();
+
+            circle.enter().append('circle')
+              .attr('cx', function (d) { return x(d[0]); })
+              .attr('cy', function (d) { return y(d[1]); })
+              .attr('fill', function (d) { return fill(psql(d[2][1]) / psql(d[2][0])); })
+              .attr('r', function (d) { return r(psql(d[2][0])); });
+
+            svg.attr('width', Math.max(320 - marginTop - rBottom - marginBottom + rRight, gXAxis.node().getBBox().width) - marginLeft + rLeft);
+
+            g.attr('transform', 'translate(' + (rLeft - marginLeft) + ',' + marginTop + ')');
+
+            // clientCount
 
             if (!clientCount)
             {
               clientCount = d3.select('body').append('div').attr('id', 'clientCount');
             }
 
-            clientCount.classed('disconnected', data[2] < 1);
+            clientCount.classed('disconnected', data[3] < 1);
 
-            if (data[2] == 1)
+            if (data[3] == 1)
             {
               clientCount.text('1 connected log collation client');
             }
             else
             {
-              clientCount.text(d3.format(',')(data[2]) + ' connected log collation clients');
+              clientCount.text(d3.format(',')(data[3]) + ' connected log collation clients');
             }
           });
 
